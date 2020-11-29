4bba0002122370270294704e802c4b6127841036
Stephen Clements <mrevilg@gmail.com>
2020-11-29 08:12:21 -0600

    argparse

--- standardLib.py
+++ standardLib.py
@@ -1,3 +1,5 @@
 # List of commonly used Standard Libaries in Python
 
 atexit # Allows you to register functions for your program to call when exiting.
+
+argparse # Provides functions for parsing command line arguments.
