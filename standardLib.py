312043d4c51bc33d29013e51b03ee9b4a61ac354
Stephen Clements <mrevilg@gmail.com>
2020-11-30 06:14:32 -0600

    bisect

--- standardLib.py
+++ standardLib.py
@@ -3,3 +3,5 @@
 atexit # Allows you to register functions for your program to call when exiting.
 
 argparse # Provides functions for parsing command line arguments.
+
+bisect # provides bisection algorithms for sorting lists.
