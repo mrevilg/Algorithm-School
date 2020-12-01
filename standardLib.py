57d0c72a58259b60f155b3beb545fa28a5e0c9d5
Stephen Clements <mrevilg@gmail.com>
2020-12-01 07:51:01 -0600

    calendar

--- standardLib.py
+++ standardLib.py
@@ -5,3 +5,5 @@ atexit # Allows you to register functions for your program to call when exiting.
 argparse # Provides functions for parsing command line arguments.
 
 bisect # provides bisection algorithms for sorting lists.
+
+calendar # Provides a number of date related functions.
