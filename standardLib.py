956ea9423ddc82782f7bf098c15230d763bf9c7f
Stephen Clements <mrevilg@gmail.com>
2020-12-02 10:06:54 -0600

    codecs

--- standardLib.py
+++ standardLib.py
@@ -7,3 +7,5 @@ argparse # Provides functions for parsing command line arguments.
 bisect # provides bisection algorithms for sorting lists.
 
 calendar # Provides a number of date related functions.
+
+codecs # Provides functions for encoding and decoding data.
