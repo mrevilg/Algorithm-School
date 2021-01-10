b8a5126040d6e35b13cf4de3054001eba1a02304
Stephen Clements <mrevilg@gmail.com>
2021-01-10 17:19:33 -0600

    Exception

--- stacked_decorators.py
+++ stacked_decorators.py
@@ -4,3 +4,4 @@ def check_user_in_not(username):
     def check_user_decorator(f):
         def wrapper(*args, **kwargs):
             if kwargs.get('username') == username:
+                raise Exception("This user is not allowd to get food")
