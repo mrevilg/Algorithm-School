64bada6466645ec957b3975131a0856dcf8fb596
Stephen Clements <mrevilg@gmail.com>
2021-01-26 08:02:14 -0600

    tree var

--- tree.py
+++ tree.py
@@ -6,3 +6,5 @@ class ReplaceBinOp(ast.NodeTransformer):
     """Replace operation by addition in binary operation"""
     def visit_BinOp(self, node):
         return ast.BinOp(left=node.lefty, op=ast.Add(), right=node.right)
+
+tree = ast.parse("x = 1/3")
